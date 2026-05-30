bf3bbee964be24fff95c91f85ea7c887ae11235e
Karan Visani <karanvisani719@gmail.com>
2026-05-30 15:43:53 +0530

    Debug API URL

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+console.log(import.meta.env.VITE_API_URL)
 const API = axios.create({
     // baseURL: "http://localhost:3000/api"
     baseURL: import.meta.env.VITE_API_URL
